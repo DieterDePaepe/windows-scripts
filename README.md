6d57827ca90f5e6fa62624ae60dad1e3092e65ee
Dieter De Paepe <dieter.depaepe@gmail.com>
2016-04-13 22:54:21 +0200

    Add warp script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# windows-scripts
-A collection of scripts for windows
+# Windows Scripts
+A collection of useful scripts for windows.
+
+Install by checking out the code and adding the scripts to your `PATH` environment.
+
+## Warp
+A folder bookmarker for use in the terminal.
+
+    c:\Temp>warp /create temp     # Create a new bookmark
+    Created bookmark "temp"
+    c:\Temp>cd c:\Users\Public    # Go somewhere else
+    c:\Users\Public>warp temp     # Go to the stored bookmark
+    c:\Temp>
+
---
+++ warp.bat
@@ -0,0 +1,55 @@
+@ECHO OFF
+REM Source found on https://github.com/DieterDePaepe/windows-scripts
+REM Please share any improvements made!
+
+REM Folder where all links will end up
+set WARP_REPO=%USERPROFILE%\.warp
+
+IF [%1]==[/?] GOTO :help
+IF [%1]==[--help] GOTO :help
+IF [%1]==[/create] GOTO :create
+IF [%1]==[/remove] GOTO :remove
+IF [%1]==[/list] GOTO :list
+
+set /p WARP_DIR=<%WARP_REPO%\%1
+cd %WARP_DIR%
+GOTO :end
+
+:create
+IF [%2]==[] (
+  ECHO Missing name for bookmark
+  GOTO :EOF
+)
+
+if not exist %WARP_REPO%\NUL mkdir %WARP_REPO%
+ECHO %cd% > %WARP_REPO%\%2
+ECHO Created bookmark "%2"
+GOTO :end
+
+:list
+dir %WARP_REPO% /B
+GOTO :end
+
+:remove
+IF [%2]==[] (
+  ECHO Missing name for bookmark
+  GOTO :EOF
+)
+if not exist %WARP_REPO%\%2 (
+  ECHO Bookmark does not exist: %2
+  GOTO :EOF
+)
+del %WARP_REPO%\%2
+GOTO :end
+
+:help
+ECHO Create or navigate to folder bookmarks.
+ECHO.
+ECHO   warp /?			Display this help
+ECHO   warp [bookmark]		Navigate to existing bookmark
+ECHO   warp /remove [bookmark]	Remove an existing bookmark
+ECHO   warp /create [bookmark]	Navigate to existing bookmark
+ECHO   warp /list			List existing bookmarks
+ECHO.
+
+:end
