8646cc6136f86d48f0c4c83912713c3c17b11458
Dieter De Paepe <dieter.depaepe@gmail.com>
2016-04-18 01:03:14 +0200

    Add /window command.

--- warp.bat
+++ warp.bat
@@ -10,12 +10,22 @@ set WARP_COMMAND_ISSUED=%1
 IF [%1]==[/?] GOTO :help
 IF [%1]==[--help] GOTO :help
 IF [%1]==[/create] GOTO :create
-IF [%1]==[/remove] GOTO :remove
 IF [%1]==[/list] GOTO :list
+IF [%1]==[/remove] GOTO :remove
+IF [%1]==[/window] GOTO :explorer
 IF [%WARP_COMMAND_ISSUED:~0,1%]==[/] GOTO :unknowncommand
 
 set /p WARP_DIR=<%WARP_REPO%\%1
 cd %WARP_DIR%
+GOTO :end
+
+:explorer
+IF [%2]==[] (
+  ECHO Missing name
+  GOTO :EOF
+)
+set /p WARP_DIR=<%WARP_REPO%\%2
+START explorer %WARP_DIR%
 GOTO :end
 
 :create
@@ -54,9 +64,10 @@ ECHO Create or navigate to folder bookmarks.
 ECHO.
 ECHO   warp /?			Display this help
 ECHO   warp [bookmark]		Navigate to existing bookmark
-ECHO   warp /remove [bookmark]	Remove an existing bookmark
 ECHO   warp /create [bookmark]	Navigate to existing bookmark
 ECHO   warp /list			List existing bookmarks
+ECHO   warp /remove [bookmark]	Remove an existing bookmark
+ECHO   warp /window [bookmark]	Open an existing bookmark in explorer
 ECHO.
 
 :end
