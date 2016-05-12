1134659f53d56775d724770de809b8c249f23e31
Dieter De Paepe <dieter.depaepe@gmail.com>
2016-05-13 00:58:13 +0200

    Return exitcode 1 on error.

--- warp.bat
+++ warp.bat
@@ -22,32 +22,36 @@ IF [%1]==[/window] GOTO :explorer
 IF [%WARP_COMMAND_ISSUED:~0,1%]==[/] GOTO :unknowncommand
 
 REM Command: warp <bookmark>
+if not exist %WARP_REPO%\%1 (
+  ECHO Bookmark does not exist: %1
+  exit /b 1
+)
 set /p WARP_DIR=<%WARP_REPO%\%1
-endlocal && pushd %WARP_DIR% && echo %WARP_DIR% > %WARP_CURRENT% && GOTO :end
+endlocal && pushd %WARP_DIR% && echo %WARP_DIR% > %WARP_CURRENT% && GOTO :eof
 
 :current
 set /p WARP_DIR=<%WARP_CURRENT%
-endlocal && pushd %WARP_DIR% && GOTO :end
+endlocal && pushd %WARP_DIR% && GOTO :eof
 
 :explorer
 IF [%2]==[] (
   ECHO Missing name
-  GOTO :EOF
+  exit /b 1
 )
 set /p WARP_DIR=<%WARP_REPO%\%2
 START explorer %WARP_DIR%
-GOTO :end
+GOTO :eof
 
 :create
 IF [%2]==[] (
   ECHO Missing name for bookmark
-  GOTO :EOF
+  exit /b 1
 )
 
 if not exist %WARP_REPO%\NUL mkdir %WARP_REPO%
 ECHO %cd% > %WARP_REPO%\%2
 ECHO Created bookmark "%2"
-GOTO :end
+GOTO :eof
 
 :list
 REM Delayed expansion needed to change variable inside the loop.
@@ -58,23 +62,23 @@ for %%f in (%WARP_REPO%\*) do (
     echo %%~nf: !LOCATION!
 )
 setlocal disabledelayedexpansion
-GOTO :end
+GOTO :eof
 
 :unknowncommand
 ECHO Unknown Warp command: %1
-GOTO :end
+exit /b 1
 
 :remove
 IF [%2]==[] (
   ECHO Missing name for bookmark
-  GOTO :EOF
+  exit /b 1
 )
 if not exist %WARP_REPO%\%2 (
   ECHO Bookmark does not exist: %2
-  GOTO :EOF
+  exit /b 1
 )
 del %WARP_REPO%\%2
-GOTO :end
+GOTO :eof
 
 :help
 ECHO Create or navigate to folder bookmarks.
@@ -87,5 +91,3 @@ ECHO   warp /list			List existing bookmarks
 ECHO   warp /remove [bookmark]	Remove an existing bookmark
 ECHO   warp /window [bookmark]	Open an existing bookmark in explorer
 ECHO.
-
-:end
